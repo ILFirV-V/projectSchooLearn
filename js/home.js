62682e45673618c8a2ccf6b3622729e797945642
Сахабутдинов Ильфир <ilfir.sakhabutdinov@bk.ru>
2022-12-22 23:27:56 +0500

    работы с формой в js

--- js/home.js
+++ js/home.js
@@ -20,3 +20,28 @@ function closeForm(event) {
 //     btnOpenInputForm.disabled = true;
 // }
 
+const applicantForm = document.getElementById('form__box')
+applicantForm.addEventListener('submit', handleFormSubmit)
+
+function serializeForm(formNode) {
+    return new FormData(formNode);
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault()
+    const data = serializeForm(applicantForm)
+    console.log(data)
+    sendData(data)
+}
+
+async function sendData(data) {
+    const headers = {
+        'Content-Type': 'application/json'
+    }
+    return await fetch('https://localhost:7238/api/Token', {
+        method: 'POST',
+        headers: headers,
+        body: data,
+    })
+}
+
