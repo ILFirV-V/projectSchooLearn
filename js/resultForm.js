b072f321c3717d20939fb91be577c5dd3a8db072
Сахабутдинов Ильфир <ilfir.sakhabutdinov@bk.ru>
2023-01-12 23:53:46 +0500

    предварительный вариант v6

--- js/resultForm.js
+++ js/resultForm.js
@@ -8,10 +8,12 @@ participateBtn.addEventListener('click', async () => {
     if (participateBtn.disabled === false) {
         participateForm.style.display = "block";
         participateBtn.disabled = true;
+    } else {
+
     }
 });
 function participateCloseForm(event) {
-    if(event.target.closest('.results_btn_box') === null && participateBtn.disabled){
+    if(event.target.closest('.results_btn_box') === null){
         participateForm.style.display = 'none';
         participateBtn.disabled = false;
     }
